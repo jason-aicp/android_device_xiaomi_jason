21e396b229d1a06acaf0cafce33b515f0e58bc3e
Dušan Uverić <dusan.uveric9@gmail.com>
2020-04-12 11:11:14 +0200

    jason: add vibrator service
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- device.mk
+++ device.mk
@@ -178,3 +178,8 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     readmac
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
